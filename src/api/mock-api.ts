bc27a0ba716a024f8d0738be36352197620c0477
Ali Sasani <alisasanibab@gmail.com>
2022-10-26 22:53:42 +0330

    update apis

--- src/api/mock-api.ts
+++ src/api/mock-api.ts
@@ -2,17 +2,14 @@ export type Profile = {
   firstName: string;
   lastName: string;
   dob: string;
-  profilePictureURL: string | null;
-  salary: number;
-  // add in any other fields that make sense for a user profile
+  profilePictureURL: string;
 };
 
 let profile: Profile = {
-  firstName: "Dua",
-  lastName: "Clipa",
-  dob: "1995-08-22",
-  profilePictureURL: null,
-  salary: 40000,
+  firstName: 'Dua',
+  lastName: 'Clipa',
+  dob: '1995-08-22',
+  profilePictureURL: 'https://i.postimg.cc/V6BRPmnx/profile.png',
 };
 
 export function getUserProfile() {
@@ -22,18 +19,53 @@ export function getUserProfile() {
 }
 
 export function setUserProfile(newProfile: Profile) {
-  return new Promise<"OK">((resolve) => {
+  return new Promise<'OK'>((resolve) => {
     setTimeout(() => {
-      const { firstName, lastName, dob, profilePictureURL, salary } =
-        newProfile;
+      const { firstName, lastName, dob, profilePictureURL } = newProfile;
       profile = {
         firstName,
         lastName,
         dob,
         profilePictureURL,
-        salary,
       };
-      resolve("OK");
+      resolve('OK');
+    }, 2e3);
+  });
+}
+
+//////////////////
+
+export type Contract = {
+  salary: number;
+  bonus: number;
+  startDate: string;
+  leaveDays: number;
+};
+
+let contract: Contract = {
+  salary: 40000,
+  bonus: 10000,
+  startDate: '2021-01-19',
+  leaveDays: 12,
+};
+
+export function getUserContract() {
+  return new Promise<Contract>((resolve) => {
+    setTimeout(() => resolve(contract), 1e3);
+  });
+}
+
+export function setUserContract(newContract: Contract) {
+  return new Promise<'OK'>((resolve) => {
+    setTimeout(() => {
+      const { salary, bonus, startDate, leaveDays } = newContract;
+      contract = {
+        salary,
+        bonus,
+        startDate,
+        leaveDays,
+      };
+      resolve('OK');
     }, 2e3);
   });
 }
