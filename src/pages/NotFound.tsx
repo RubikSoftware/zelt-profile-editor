0d5171abcae57032f6f1d81336ceaf8fd724e94b
Ali Sasani <alisasanibab@gmail.com>
2022-10-26 23:01:34 +0330

    add NotFound component

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const NotFound = () => {
+  return (
+    <Typography sx={{ mt: 10 }} variant="h4">
+      Not Found :(
+    </Typography>
+  );
+};
+
+export default NotFound;
