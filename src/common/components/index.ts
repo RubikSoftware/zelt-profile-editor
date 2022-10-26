44264805d72a98ee9b9df17e9b86b6226aa7da53
Ali Sasani <alisasanibab@gmail.com>
2022-10-26 22:55:28 +0330

    add Personal page and its related components

---
+++ src/common/components/index.ts
@@ -0,0 +1,5 @@
+export { default as ActionButtons } from './ActionButtons';
+export { default as CustomizedField } from './CustomizedField';
+
+export { default as Navbar } from './Navbar';
+export { default as PersonalDataHeader } from './PersonalDataHeader';
